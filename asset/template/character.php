a155f5ef87a54906ad221be18c45aa21d85edcd7
antoine-codeur <antoinejf.denoue@gmail.com>
2024-03-12 13:55:56 +0100

    correction

--- asset/template/character.php
+++ asset/template/character.php
@@ -1,7 +1,7 @@
 <div class="character-card">
+    <h2><?php echo $characterName; ?></h2>
     <div class="img-container">
         <img src="<?php echo $characterImage; ?>" alt="Portait de <?php echo $characterName; ?>" loading="lazy">
     </div>
-    <h2><?php echo $characterName; ?></h2>
     <p><?php echo $characterHouse; ?></p>
 </div>

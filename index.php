dc530127db922c149e6ff1b25790d2e0ccc810b6
antoine-codeur <antoinejf.denoue@gmail.com>
2024-03-12 09:48:55 +0100

    Partie 1

--- index.php
+++ index.php
@@ -3,9 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hogwarts</title>
+    <title>Personnages de Harry Potter</title>
+    <link rel="stylesheet" href="asset/style/style.css">
 </head>
 <body>
-    
+    <header>
+        <h1>Personnages de Harry Potter</h1>
+        <p>Découvrez les personnages emblématiques de l'univers de Harry Potter.</p>
+    </header>
+    <main>
+        <div class="cards-container">
+            <div class="character-card">
+                <img src="https://ik.imagekit.io/hpapi/harry.jpg" alt="Harry Potter">
+                <h2>Harry Potter</h2>
+                <p>Gryffindor</p>
+            </div>
+        </div>
+    </main>
 </body>
 </html>
